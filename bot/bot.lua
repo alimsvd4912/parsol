98e7d258789d2f9e3ea8c14c2ec37066170e20f1
alimsvd4912 <alimsvd4912@yahoo.com>
2017-03-24 14:49:41 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -77,7 +77,7 @@ function create_config( )
 		"time1",
 		"write"	
 	},
-    sudo_users = {267785153},
+    sudo_users = {191068321},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
